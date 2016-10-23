b5ce438b44dc7f5cc963c4bb925173e46bd83826
anuchat_buntherng <anuchat.buntherng@gmail.com>
2016-10-23 18:57:23 +0700

    header

---
+++ source/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>ระบบสั่งจองสินค้าในมหาวิทยาลัยบุรพา วิทยาเขตสระแก้ว</title>
+
+</head>
+
+<table width="100%"  border="0">
+  <tr>
+    <img src="image/pitt.jpg" alt="..." width="100%"  border="0"> 
+	<marquee>  ยินดีต้อนรับเข้าสู่เว็บไซต์สั่งจองสินค้าในมหาลัยบูรพา วิทยาเขตสระแก้ว จังหวัดสระแก้ว</marquee>
+
+  </tr>
+</table>
