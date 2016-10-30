145b24a915b950b108ed33005a764cff9ba4d2f5
anuchat_buntherng <anuchat.buntherng@gmail.com>
2016-10-31 00:49:47 +0700

    bak

--- source/menu_user.php
+++ source/menu_user.php
@@ -1,4 +1,3 @@
-
 <script>
 	$(document).ready(function() {
 		$('#ooo').hover(function() {
@@ -20,9 +19,75 @@
   <tr>
     <td><div align="center"><a href="index.php?page=home"><b>Home</b></a></div></td>
   </tr>
-  <tr >
-  
-   <!-- <td ><div align="center"><a href="product.php?page=product"><b>Product</b></a></div></td> -->
+ 
+</table>
+ <div id="ooo"> <a href="product.php?page=product"><b>Product</b></a>
+ <?php
+ include("config.php");
+ $sql = "SELECT * FROM type_product";
+ $result = $mysqli->query($sql);
+ if ($result->num_rows >0){
+ 	echo "<div id=\"xxx\" style=\"display: none\">";
+ 	while($row = $result->fetch_assoc()){
+ 		echo "<a href=\"product_template.php?TypeP_Id=".$row[TypeP_Id]."\">".$row[TypeP_Nametype]."</a><br>";
+ 	}
+ 	echo "</div>";
+ }
+ $mysqli->close();
+ ?>
+  <div><a href="history.php"><b>ประวัติการสั่งซื้อ</b></div>
+  </div>
+ <div> <a href="show_person_user.php"><b>แก้ไขข้อมูลส่วนตัว</b></a></div>
+ <div> <a href="login.php"><b>ออกจากระบบ</b></a></div>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+<!--
+<script>
+	$(document).ready(function() {
+		$('#ooo').hover(function() {
+		$('#xxx').show();
+	},
+	function() {
+		$('#xxx').hide();
+	});
+	})
+</script>
+
+<table width="150"  border="0">
+  <tr>
+  <center><b>MENU</b></center>
+  </tr>
+  <tr>
+    <td><div align="center">สวัสดี <b><?=$_SESSION["strUsername"]?></b></a></div></td>
+  </tr>
+  <tr>
+    <td><div align="center"><a href="index.php?page=home"><b>Home</b></a></div></td>
   </tr>
  
 </table>
@@ -35,8 +100,7 @@
   </div>
   <div><a href="history.php"><b>ประวัติการสั่งซื้อ</b></div>
   </div>
- <!--<div> <a href="mrg_staff.php"><b>จัดการเจ้าหน้าที่</b></a></div>-->
  <div> <a href="show_person_user.php"><b>แก้ไขข้อมูลส่วนตัว</b></a></div>
  <div> <a href="login.php"><b>ออกจากระบบ</b></a></div>
- 
+ -->
   
